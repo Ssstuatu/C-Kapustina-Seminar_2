62449af622ca1663fa1d64a65ebc0e1df553c4c0
sofia <ssstuatu1405@gmail.com>
2023-07-16 16:21:13 +0700

    add Задача_23

---
+++ GeekBrains Разработчик/Знакомство с языками программирования С#. Домашнее задание 3/Задача_23/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void DefSqr (int num)
+{
+        Console.WriteLine($"{num} => ");
+        for (int i = 1; i < num; i++)
+        {
+            Console.Write(i * i * i + ", ");
+        }
+        Console.WriteLine(num * num * num + ".");
+}
+int GetInfo(string message)
+{
+    Console.Write(message);
+    int inputNumber = Convert.ToInt32(Console.ReadLine());
+    return inputNumber;
+}
+int num = GetInfo("Введите число: ");
+DefSqr(num);
